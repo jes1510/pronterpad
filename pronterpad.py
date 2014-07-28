1777e35042580e6b44a1fdd99397d23feb71fd88
jes1510 <jes1510@gmail.com>
2014-07-28 00:06:03 -0400

    Having bad storms so I'm doing an early commit just in case Added communications wrapper class for G-Code sending Added speed box controls for axes

--- pronterpad.py
+++ pronterpad.py
@@ -3,16 +3,20 @@ import pygame
 import wx
 import time
 
+
+
+
 pygame.init()
 
-
-#wx.EVT_TIMER(panel, TIMER_ID, on_timer)  # call the on_timer function
-
 class pronterPadFrame(wx.Frame):
+  
+    
     def __init__(self, *args, **kwds):
         # begin wxGlade: pronterPadFrame.__init__
-        kwds["style"] = wx.DEFAULT_FRAME_STYLE
-        wx.Frame.__init__(self, *args, **kwds)
+        self.port = None
+        kwds["style"] = wx.DEFAULT_FRAME_STYLE        
+        wx.Frame.__init__(self, *args, **kwds)   
+
         self.TIMER_ID = 100  # pick a number
         
         self.controller = Joystick()
@@ -24,6 +28,7 @@ class pronterPadFrame(wx.Frame):
         self.numButtons = self.controller.countButtons()
         
         self.axesList = [0.0] * self.numAxes 
+        self.axesSpeeds = [] * self.numAxes
         self.buttonsList = [0] * self.numButtons     
         
         self.axesLabels = []  
@@ -34,16 +39,19 @@ class pronterPadFrame(wx.Frame):
         for i in range(0, self.numAxes) :
             text = 'Axis ' + str(i) + ': 0.0'
             self.axesLabels.append(wx.StaticText(self.panel_1, wx.ID_ANY, text))
+            self.axesSpeeds.append(wx.TextCtrl(self.panel_1, value='10'))
             
+          
+        print self.axesSpeeds
         for i in range(0, self.numButtons) :
             text = 'Button ' + str(i) + ': 0'
-            self.buttonsLabels.append(wx.StaticText(self.panel_1, wx.ID_ANY, text))
-        
+            self.buttonsLabels.append(wx.StaticText(self.panel_1, wx.ID_ANY, text))        
 
         self.Bind(wx.EVT_TIMER, self.on_timer, self.timer)     
         self.__set_properties()
         self.__do_layout()
         # end wxGlade
+        
 
     def __set_properties(self):
         # begin wxGlade: pronterPadFrame.__set_properties
@@ -56,7 +64,8 @@ class pronterPadFrame(wx.Frame):
         sizer_2 = wx.BoxSizer(wx.VERTICAL)
         
         for i in range(0, self.numAxes) :         
-            sizer_2.Add(self.axesLabels[i], 0, 0, 0)
+            sizer_2.Add(self.axesLabels[i], 0, 0, 0)   
+            sizer_2.Add(self.axesSpeeds[i], 0, 0, 0)
             
         for i in range(0, self.numButtons) :
             sizer_2.Add(self.buttonsLabels[i], 0, 0, 0)
@@ -78,12 +87,15 @@ class pronterPadFrame(wx.Frame):
         data = self.controller.readButtons()  
         for i in range(0, self.numButtons) :
             out = 'Button ' + str(i) + ': ' + str(data[i])
-            self.buttonsLabels[i].SetLabel(out)
-
+            self.buttonsLabels[i].SetLabel(out)    
+        
+        print com.port
+    
+    
     
     
 class Joystick() :
-    def __init__ (self):
+    def __init__ (self, *args, **kwds):
         self.joy = pygame.joystick.Joystick(0)
         self.joy.init()        
         
@@ -112,17 +124,36 @@ class Joystick() :
         return out
    
 
-def sendGCode(axis, steps) :
-    line = 'G91 ' + axis + ' ' + str(steps) + ' G90'
-    print line
+        
+class Com() :
+    def __init__(self, *args, **kwds) :
+        self.port = None       
+        
+    def _send(self, data) :
+        port.write(data)
+        
+    def sendGCode(axis, steps) :
+        line = 'G91 ' + axis + ' ' + str(steps) + ' G90'
+        self._send(line)
+        
+    def setCom(self, *args, **kwds) :
+        self.port = kwds['port']
+
 
 # end of class pronterPadFrame
 if __name__ == "__main__":  
+    import serial
+    ser = serial.Serial('/dev/ttyS0', 115200)  
+    com = Com()
+    com.setCom(port=ser)  
+    
     app = wx.PySimpleApp(0)
     wx.InitAllImageHandlers()
-    frame_1 = pronterPadFrame(None, wx.ID_ANY, "")
-    app.SetTopWindow(frame_1)
-    frame_1.Show()
+ 
+    ppFrame = pronterPadFrame(None, wx.ID_ANY, '')
+    app.SetTopWindow(ppFrame)
+    ppFrame.Show() 
+    
     app.MainLoop()
 
 
